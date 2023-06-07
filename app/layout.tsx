6b4f344feed540350a053e5bc8c3d5686afabcdc
Jesse Emana <jesseemana@gmail.com>
2023-06-07 22:31:57 +0200

    added app container

---
+++ app/layout.tsx
@@ -0,0 +1,29 @@
+// import { Nunito } from 'next/font/google'
+
+import Navbar from './components/Navbar';
+import './globals.css'
+
+export const metadata = {
+  title: 'Airbnb',
+  description: 'Airbnb Clone',
+}
+
+// const font = Nunito({ 
+//   subsets: ['latin'], 
+// });
+
+export default async function RootLayout({ children }: { children: React.ReactNode }) {
+  // const currentUser = await getCurrentUser();
+
+  return (
+    <html lang="en">
+      <body className=''>
+      {/* <body className={font.className}> */}
+        <div className="">
+          <Navbar />
+          {children}
+        </div>
+      </body>
+    </html>
+  )
+}
