a112cd465fa57cb6b8146406d751262f3e7e80b1
Jesse Emana <jesseemana@gmail.com>
2023-06-08 18:12:39 +0200

    moved the types to a separate types folder

--- app/components/Navbar/UserMenu.tsx
+++ app/components/Navbar/UserMenu.tsx
@@ -2,8 +2,15 @@
 
 import { AiOutlineMenu } from 'react-icons/ai'
 import Avatar from '../Avatar'
+import { useState, useCallback } from 'react'
 
 const UserMenu = () => {
+  const [isOpen, setIsOpen] = useState(false)
+
+  const toggleOpen = useCallback(() => {
+    setIsOpen((value) => !value)
+  }, [])
+
   return (
     <div className='relative'>
         <div className='flex flex-row items-center gap-3'>
@@ -14,7 +21,7 @@ const UserMenu = () => {
                 Add Your Home
             </div>
             <div 
-                onClick={() => {}} 
+                onClick={toggleOpen} 
                 className='py-4 border-[1px] md:py-1 md:px-3 rounded-full hover:bg-neutral-200 flex flex-row items-center transition cursor-pointer'
             >
                 <AiOutlineMenu />
@@ -23,6 +30,15 @@ const UserMenu = () => {
                 </div>
             </div>
         </div>
+
+        {isOpen && (
+            <div className='absolute rounded-xl shadow-md w-[40vw] md:w-3/4 bg-white overflow-hidden right-0 top-12 text-sm'
+            >
+                <div className=''>
+
+                </div>
+            </div>
+        )}
     </div>
   )
 }
