7a2fb1f4c7962469188288e3217ab5069cf04680
Jesse Emana <jesseemana@gmail.com>
2023-06-14 22:30:19 +0200

    modified code

--- app/components/Container.tsx
+++ app/components/Container.tsx
@@ -1,11 +1,11 @@
-"use client"
+'use client'
 
 const Container: React.FC<ContainerProps> = ({children}) => {
   return (
-    <div className="max-w-[2520px] mx-auto xl:px-20 md:px-10 sm:px-2 px-4">
+    <div className="max-w-[2480px] mx-auto xl:px-20 md:px-10 sm:px-2 px-4">
       {children}
     </div>
   )
 }
 
-export default Container    
+export default Container  
