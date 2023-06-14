97cf2149eaad818fc6b3a97ea64b5856b7c8b8f7
Jesse Emana <jesseemana@gmail.com>
2023-06-15 00:37:31 +0200

    created a hook and component for selecting countries

--- app/components/Modals/RentModal.tsx
+++ app/components/Modals/RentModal.tsx
@@ -1,17 +1,51 @@
 'use client'
 
 import Modal from './Modal'
+import { useState, useMemo } from 'react'
 import useRentalModal from '@/app/hooks/useRentalModal'
+
+enum STEPS {
+    CATEGORY = 0,
+    LOCATION= 1,
+    INFO = 2,
+    IMAGES = 3,
+    DESCRIPTION = 4,
+    PRICE = 5
+}
 
 const RentModal = () => {
     const rentModal = useRentalModal()
+
+    const [step, setStep] = useState(STEPS.CATEGORY)
+
+    function onBack() {
+        setStep(value => value - 1)
+    }
+
+    function onNext() {
+        setStep(value => value + 1)
+    }
+
+    const actionLabel = useMemo(() => {
+        if(step === STEPS.PRICE) return 'Create'
+
+        return 'Next'
+    }, [step])
+
+    const secondaryActionLabel = useMemo(() => {
+        if(step === STEPS.CATEGORY) return undefined
+
+        return 'Back'
+    }, [step])
 
     return(
         <Modal
             isOpen={rentModal.isOpen}
             onClose={rentModal.onClose}
             onSubmit={rentModal.onClose}
-            actionLabel='Submit'
+            actionLabel={actionLabel}
+            secondaryActionLabel={secondaryActionLabel}
+            secondaryAction={steps === STEPS.CATEGORY ? undefined : onBack}
             title='AirBnB Your Home'
         />
     )
