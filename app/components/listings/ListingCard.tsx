03c39f44ac1cb0e321f6fe52751fedfc34c31a5d
Jesse Emana <jesseemana@gmail.com>
2023-06-16 19:54:10 +0200

    added styling and functionality for the listing card

--- app/components/listings/ListingCard.tsx
+++ app/components/listings/ListingCard.tsx
@@ -3,7 +3,9 @@
 import useCountries from '@/app/hooks/useCountries';
 import { SafeUser, SafeListing, SafeReservation } from '@/app/types'
 import { useRouter } from 'next/navigation' 
-
+import { useMemo } from 'react'
+import { format } from 'date-fns'
+import Image from 'next/image'
 
 interface ListingCardProps {
   data: SafeListing
@@ -37,9 +39,38 @@ const ListingCard: React.FC<ListingCardProps> = ({
 
     onAction?.(actionId)
   }
+
+  const price = useMemo(() => {
+    if(reservation) return reservation?.totalPrice
+
+    return data.price
+  }, [reservation, data.price])
+
+  const reservationDate = useMemo(() => {
+    if(!reservation) return null
+
+    const start = new Date(reservation.startDate)
+    const end = new Date(reservation.endDate)
+
+    return `${format(start, 'PP')} - ${format(end, 'PP')}`
+  }, [reservation])
   
   return (
-    <div>Card</div>
+    <div 
+      onClick={() => router.push(`/listings/${data.id}`)}
+      className='cols-span-1 cursor-pointer group'
+    >
+      <div className='flex flex-col gap-2 w-full'>
+        <div className='aspect-square w-full relative overflow-hidden rounded-xl'>
+          <Image 
+          fill
+          className='object-cover h-full w-full group-hover:scale-110 transition'
+          src={data.imageSrc}
+          alt='Listing'
+          />
+        </div>
+      </div>
+    </div>
   )
 }
 
